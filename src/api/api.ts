cba5e899f1ffa229e5b2025f423cc43236e582ee
Symbosys <symbosystech@gmail.com>
2026-04-17 14:49:15 +0530

    change the cors origin

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from "../store/authStore";
 const api = axios.create({
     baseURL: window.location.hostname === "localhost" 
         ? "http://localhost:4000/api/v1" 
-        : "https://urban-classes-backend.vercel.app/api/v1",
+        : "https://urban-calsses-backend.vercel.app/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
